17c49d09d78bc3675a466abb90e11fd184d1835e
AllenD <allengdiaz2019@gmail.com>
2025-11-17 21:36:17 +0800

    Solve: build pyramid shape tower problem

---
+++ 11-17-25-buildPyramidShapeTower.js
@@ -0,0 +1,42 @@
+// problem
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+
+//Solution
+function towerBuilder(nFloors) {
+  let tower = [];
+  for (let i = 1; i <= nFloors; i++) {
+    let spaces = ' '.repeat(nFloors - i);
+    let stars = '*'.repeat(2 * i - 1);
+    tower.push(spaces + stars + spaces);
+  }
+  return tower;
+}
+
+//Test
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertDeepEquals(towerBuilder(1), ["*"]);
+Test.assertDeepEquals(towerBuilder(2), [" * ","***"]);
+Test.assertDeepEquals(towerBuilder(3), ["  *  "," *** ","*****"]);
+  });
+});
